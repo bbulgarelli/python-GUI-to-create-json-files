167e49bfcc35763eddfb4d3c332696085f06bc8e
fernandohjunqueira <fernandohjunqueira@outlook.com>
2020-05-05 16:44:08 -0300

    Add json file example and primitive reading functionality
    Signed-off-by: fernandohjunqueira <fernandohjunqueira@outlook.com>

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import tkinter
+import json
 
 #definicoes das variaveis globais
 comprimento = 600
@@ -8,7 +9,10 @@ altura = 450
 root = tkinter.Tk()#inicia a classe/janela principal
 root.title("JSON GUI")#titulo da janela principal
 root.geometry("{}x{}".format(comprimento, altura))#define tamanho da janela atravéz de uma string formatada
-              
-
 
 root.mainloop()
+
+with open('test.json') as json_file:
+    data = json.load(json_file)
+    for p in data:
+        print("Texto: ", data['Capitulo1']['Boas-vindas']['Bem-vindo']['Texto'])
